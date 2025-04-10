dc6e0b8671d076495ccaba0aab5aa6fcfd2392f3
hosamnajib <hosam.najib@internetx.com>
2025-04-10 14:16:57 +0200

    First Commit

---
+++ src/main/java/org/example/documentconverter1/controller.java
@@ -0,0 +1,13 @@
+package org.example.documentconverter1;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class controller {
+
+    @GetMapping
+    public String hello() {
+        return "Hello World";
+    }
+}
